0c9be271bd3c55089def90f0a25b2d91ae07bf81
Minulck <miulck@gmail.com>
2025-02-23 16:57:26 +0530

    save user credential in local storage then give role base access

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Login from "./pages/Login";
@@ -14,9 +13,19 @@ import { Users } from "./pages/Admin/Users";
 import AssetUser from "./pages/User/AssetUser";
 import AssetAdmin from "./pages/Admin/AssetAdmin";
 
-const PrivateRoute = ({ element }) => {
+const PrivateRoute = ({ element, allowedRole }) => {
   const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-  return isAuthenticated ? element : <Navigate to="/login" />;
+  const userRole = localStorage.getItem("userRole");
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (allowedRole && userRole !== allowedRole) {
+    return <Navigate to={`/${userRole}-Dashboard${userRole.charAt(0).toUpperCase() + userRole.slice(1)}`} />;
+  }
+
+  return element;
 };
 
 function App() {
@@ -25,7 +34,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
 
-        <Route path="/" element={<PrivateRoute element={<AdminLayout />} />}>
+        <Route path="/" element={<PrivateRoute element={<AdminLayout />} allowedRole="admin" />}>
           <Route index element={<DashboardAdmin />} />
           <Route path="admin-DashboardAdmin" element={<DashboardAdmin />} />
           <Route path="admin-DashboardUser" element={<DashboardUser />} />
@@ -35,14 +44,11 @@ function App() {
           <Route path="admin-users" element={<Users />} />
           <Route path="admin-asset" element={<AssetAdmin />} />
         </Route>
-        <Route path="/" element={<PrivateRoute element={<UserLayout />} />}>
+
+        <Route path="/" element={<PrivateRoute element={<UserLayout />} allowedRoles={["user", "admin"]} />}>
           <Route index element={<DashboardUser />} />
-          <Route path="user-DashboardAdmin" element={<DashboardAdmin />} />
           <Route path="user-DashboardUser" element={<DashboardUser />} />
           <Route path="user-mealcalander" element={<MealCalander />} />
-          <Route path="user-addmealtime" element={<AddMealTime />} />
-          <Route path="user-addmealtype" element={<AddMealType />} />
-          <Route path="user-users" element={<Users />} />
           <Route path="user-asset" element={<AssetUser />} />
         </Route>
       </Routes>
