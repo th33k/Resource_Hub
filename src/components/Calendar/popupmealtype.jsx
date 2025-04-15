a93394af22a241fbb8db0879938741760d8c37c3
Theekshana Udara <theekshana.uda@gmail.com>
2025-04-15 12:11:18 +0530

    Fix import path casing for PopupMealType.css in Popupmealtype component

--- src/components/Calendar/popupmealtype.jsx
+++ src/components/Calendar/popupmealtype.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import MealTypeSelect from "./MealTypeSelect";
-import '../css/Calender/Popupmealtype.css';
+import '../css/Calender/PopupMealType.css';
 
 function Popupmealtype({ open, handleClose, onAddEvent }) {
   return (
