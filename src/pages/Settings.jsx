f30ea56a96de926737a523dd4305696837360ef2
Theekshana Udara <theekshana.uda@gmail.com>
2025-05-05 23:21:33 +0530

    Refactor Settings component: implement tab navigation for Profile and Account sections

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProfileSection from '../components/Settings/ProfileSettings';
 import AccountSection from '../components/Settings/AccountSettings';
 import AdminLayout from '../layouts/Admin/AdminLayout'; 
 import UserLayout from '../layouts/User/UserLayout'; 
+import { Tabs, Tab, Box, Paper } from '@mui/material';
 
 const Settings = () => {
-
   const userRole = localStorage.getItem("userRole");
+  const [tab, setTab] = useState(0);
 
+  const handleTabChange = (event, newValue) => {
+    setTab(newValue);
+  };
 
   const renderContent = (
-    <div className="">
+    <Paper elevation={2} sx={{ width: '100%', mx: 'auto', p: 4 }}>
       <h1 className="text-2xl font-bold mb-6 text-center">Settings</h1>
-      <ProfileSection />
-      <AccountSection />
-    </div>
+      <Tabs value={tab} onChange={handleTabChange} centered sx={{ mb: 3 }}>
+        <Tab label="Profile" />
+        <Tab label="Account" />
+      </Tabs>
+      <Box>
+        {tab === 0 && <ProfileSection />}
+        {tab === 1 && <AccountSection />}
+      </Box>
+    </Paper>
   );
 
   // Conditional rendering based on role
